3b65fb1058a4728d34d569b1f18ef807faece6fd
PREM-KUMAR-D <premkumar88845@gmail.com>
2021-12-05 18:14:24 +0530

    added all the tetrominoes shapes

--- source.cpp
+++ source.cpp
@@ -1,8 +1,60 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+wstring tetromino[7];
 
-    cout<<"hello git is it working??"<<endl;
+int main()
+{
+
+    // Create assets
+    // there are 7 (1-sided) tetrominos
+    // the reason why we add L before wstring is https://stackoverflow.com/questions/402283/stdwstring-vs-stdstring (check this for reference)
+    // to print wstring use wcout
+
+    // "I" tetromino
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    //"O"  tetromino
+    tetromino[0].append(L"....");
+    tetromino[0].append(L".XX.");
+    tetromino[0].append(L".XX.");
+    tetromino[0].append(L"....");
+
+    //"T" tetromino
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L".XX.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"....");
+
+    //"J" tetromino
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L".XX.");
+    tetromino[0].append(L"....");
+
+    //"L" tetromino
+    tetromino[0].append(L".X..");
+    tetromino[0].append(L".X..");
+    tetromino[0].append(L".XX.");
+    tetromino[0].append(L"....");
+
+    //"S" tetromino
+    tetromino[0].append(L".X..");
+    tetromino[0].append(L".XX.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"....");
+
+    //"Z" tetromino
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L".XX.");
+    tetromino[0].append(L".X..");
+    tetromino[0].append(L"....");
+
+
+    wcout << "this tetromino is " << tetromino[0] << endl;
+
     return 0;
 }
